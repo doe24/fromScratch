142d8a4acb3fcc3e56436e1ac7def7dc8f8d497b
doe24 <155100391+doe24@users.noreply.github.com>
2024-01-02 23:17:24 +0700

    Update

--- colorInterminal.js
+++ colorInterminal.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * This code is inspired from colors.js
+ * https://github.com/Marak/colors.js
+ * 
+ */
 
 const colorTextInTerminal = (text) => {
   return '\033[31m' + text + '\033[39m';
